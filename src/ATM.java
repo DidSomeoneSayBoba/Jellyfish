a43a15c85942c1b01516138eb87cb5a28474c493
ShizumeruMaking <87250151+ShizumeruMaking@users.noreply.github.com>
2021-08-27 14:59:24 -0700

    added funny amogus mechanic and a consrtutor

--- src/ATM.java
+++ src/ATM.java
@@ -2,7 +2,19 @@ import java.util.HashMap;
 
 public class ATM {
 	private HashMap<String,Integer> BankIDs;
-	public void deposit(String ID,Integer amount) {
-		BankIDs.put(ID, amount);
+	public ATM()
+	{
+		BankIDs = new HashMap();
 	}
+	public void deposit(String ID,Integer amount) {
+		if(BankIDs.containsKey(ID))
+		{
+			int storage = BankIDs.remove(ID);
+			BankIDs.put(ID,amount+storage);
+		}
+		else {
+		BankIDs.put(ID, amount);
+		}
+	}
+	
 }
