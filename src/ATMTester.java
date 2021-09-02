e73224162c75575b17b23db4d1c55628e7fa22b9
zachansell <89551318+zachansell@users.noreply.github.com>
2021-09-02 13:36:08 -0700

    implemented the test code

--- src/ATMTester.java
+++ src/ATMTester.java
@@ -1,4 +1,17 @@
 
 public class ATMTester {
-	
+	public static void main (String[] args)
+	{
+		ATM testAtm = new ATM();
+		testAtm.deposit("Winfrey", 25159684372.43);
+		testAtm.deposit("Bezos", 193495330293.43);
+		testAtm.deposit("Bezos", 20394353.35);
+		testAtm.withdraw("Bezos", 3593293.66);
+		testAtm.deposit("MrTheiss",64033.55);
+		testAtm.withdraw("MrTheiss", 443964.46);
+		testAtm.checkBalance("Bezos");
+		testAtm.checkBalance("Winfrey");
+		testAtm.checkBalance("YoMama");
+		testAtm.checkBalance("MrTheiss");
+	}
 }
